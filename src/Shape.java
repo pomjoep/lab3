f83fe5a5a60c71d871fa731fae51326ebdf42856
pomjoep <59618718+pomjoep@users.noreply.github.com>
2024-04-15 21:18:23 -0700

    meow

---
+++ src/Shape.java
@@ -0,0 +1,19 @@
+package src;
+
+import java.awt.Color;
+
+public abstract class Shape {
+        protected int x;
+        protected int y;
+        protected Color color;
+
+        public Shape(int x, int y, Color color){
+            this.color = color;
+
+        }
+
+        public static int newX(){
+
+        }
+
+}
