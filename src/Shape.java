7fdaf10a461009bfbd4e935ec833f483c4422a56
pomjoep <59618718+pomjoep@users.noreply.github.com>
2024-04-16 11:16:46 -0700

    meow

--- src/Shape.java
+++ src/Shape.java
@@ -1,19 +1,44 @@
 package src;
 
 import java.awt.Color;
+import java.util.Scanner;
 
 public abstract class Shape {
-        protected int x;
-        protected int y;
-        protected Color color;
+    protected int x = 0;
+    protected int y = 0;
+    protected Color color = Color.WHITE;
 
-        public Shape(int x, int y, Color color){
-            this.color = color;
+    //constructors
+    public Shape(){};
+    public Shape(int x, int y, Color color){
+        this.color = color;
+        newX(x);
+        newY(y);
+    }
+    public Shape(Shape anotherShape){
+        x = anotherShape.x;
+        y = anotherShape.y;
+        color = anotherShape.color;
+    }
 
+    //x y checking
+    public void newX(int x){
+        if(x < 0){
+            Scanner keyboard = new Scanner(System.in);
+            System.out.println("Invalid x, enter a new one 0 or above: ");
+            newX(keyboard.nextInt());
+        } else{
+            this.x = x;
         }
-
-        public static int newX(){
-
+    }
+    public void newY(int y){
+        if(y < 0){
+            Scanner keyboard = new Scanner(System.in);
+            System.out.println("Invalid y, enter a new one 0 or above: ");
+            newY(keyboard.nextInt());
+        } else{
+            this.y = y;
         }
+    }
 
 }
